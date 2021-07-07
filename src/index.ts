9c99669d33e081beb140c442ae8d7630491f9cf6
松本　駿 <matsumoto_toshi@r.recruit.co.jp>
2021-07-07 12:45:08 +0900

    fix: Getの時にparamsを付与できるように修正

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,136 @@
-import axios, { CancelToken } from 'axios'
-import { AnyAction, Dispatch, Middleware } from 'redux'
-import { actionCreatorFactory } from 'typescript-fsa'
+import axios, { CancelToken, AxiosRequestConfig } from "axios";
+import { AnyAction, Dispatch, Middleware } from "redux";
+import { actionCreatorFactory } from "typescript-fsa";
 
 const methodMap = {
-  read: 'get',
-  create: 'post',
-  update: 'put',
-  delete: 'delete',
-}
+  read: "get",
+  create: "post",
+  update: "put",
+  delete: "delete"
+};
 
-const actionCreator = actionCreatorFactory()
-export const axiosAction = actionCreator<Payload>('EFFECT_FETCHR')
+const actionCreator = actionCreatorFactory();
+export const axiosAction = actionCreator<Payload>("EFFECT_FETCHR");
 
-type RequestType = keyof typeof methodMap
-type MethodType = 'get' | 'patch' | 'put' | 'post' | 'delete'
+type RequestType = keyof typeof methodMap;
+type MethodType = "get" | "patch" | "put" | "post" | "delete";
 type Payload<B = any, P = any> = {
-  type: RequestType
-  path: string
-  body?: B
-  params?: P
-  method: MethodType
-  cancelToken?: CancelToken
-}
-export type AxiosFetchAction = ReturnType<typeof axiosAction>
+  type: RequestType;
+  path: string;
+  body?: B;
+  params?: P;
+  method: MethodType;
+  cancelToken?: CancelToken;
+};
+export type AxiosFetchAction = ReturnType<typeof axiosAction>;
 
-const typeToMethod = (type: RequestType) => methodMap[type] as MethodType
+const typeToMethod = (type: RequestType) => methodMap[type] as MethodType;
 
-const req = async <T = any, D = any>(
-  method: MethodType,
-  url: string,
-  data: D,
-  cancelToken?: CancelToken
+const req = async <T = any, D = any, P = any>(
+  requestConfig:
+    | {
+        method: "patch" | "put" | "post";
+        url: string;
+        params: P;
+        data: D;
+        cancelToken?: CancelToken;
+      }
+    | {
+        method: "get" | "delete";
+        url: string;
+        params: P;
+        cancelToken?: CancelToken;
+      }
 ) => {
-  const response = await axios.request<{ results: T }>({
-    method,
-    url,
-    data,
-    cancelToken,
-  })
-  return { result: response.data }
-}
+  const response = await axios.request<{ results: T }>(requestConfig);
+  return { result: response.data };
+};
 
-export const read = (path: string, params = {}, cancelToken?: CancelToken) => (): AxiosFetchAction =>
+export const read = (
+  path: string,
+  params = {},
+  cancelToken?: CancelToken
+) => (): AxiosFetchAction =>
   axiosAction({
-    type: 'read',
+    type: "read",
     path,
     params,
-    method: typeToMethod('read'),
-    cancelToken,
-  })
+    method: typeToMethod("read"),
+    cancelToken
+  });
 
 export const del = (path: string) => (): AxiosFetchAction =>
   axiosAction({
-    type: 'delete',
+    type: "delete",
     path,
-    method: typeToMethod('delete'),
-  })
+    method: typeToMethod("delete")
+  });
 
 export const readByPost = <P = any>(
   path: string,
   params: P,
   cancelToken?: CancelToken
 ) => (): AxiosFetchAction => {
-  // eslint-disable-next-line immutable/no-mutation
-  axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
   return axiosAction({
-    type: 'read',
+    type: "read",
     path,
     params,
-    method: typeToMethod('create'),
-    cancelToken,
-  })
-}
+    method: typeToMethod("create"),
+    cancelToken
+  });
+};
 
 export const create = <B = any>(
   path: string,
   body: B
 ) => (): AxiosFetchAction =>
   axiosAction({
-    type: 'read',
+    type: "read",
     path,
     body,
-    method: typeToMethod('read'),
-  })
+    method: typeToMethod("read")
+  });
 
 export const update = <B = any>(
   path: string,
   body: B
 ) => (): AxiosFetchAction =>
   axiosAction({
-    type: 'update',
+    type: "update",
     path,
     body,
-    method: typeToMethod('update'),
-  })
+    method: typeToMethod("update")
+  });
 
-export const reduxEffectsAxios: Middleware = () => (
-  next: Dispatch<AnyAction>
-) => async (action: AnyAction | AxiosFetchAction) => {
-  if (action.type !== axiosAction.type) return next(action)
-  const { method, path, body, params, cancelToken } = action.payload
+type DefaultsConfig = Pick<AxiosRequestConfig, "headers">;
+export const reduxEffectsAxios = (
+  defaults: DefaultsConfig = { headers: {} }
+): Middleware => {
+  axios.defaults.headers = {
+    ...axios.defaults.headers,
+    ...defaults.headers
+  };
+  return () => (next: Dispatch<AnyAction>) => async (
+    action: AnyAction | AxiosFetchAction
+  ) => {
+    if (action.type !== axiosAction.type) return next(action);
+    const { method, path, body, params, cancelToken } = action.payload;
 
-  if (method === 'get' || method === 'delete') {
-    return await req(method, path, params, cancelToken)
-  } else {
-    return await req(method, path, params || body, cancelToken)
-  }
-}
+    if (method === "get" || method === "delete") {
+      return req({
+        method,
+        url: path,
+        params,
+        cancelToken
+      });
+    } else {
+      return req({
+        method,
+        url: path,
+        params,
+        data: body,
+        cancelToken
+      });
+    }
+  };
+};
